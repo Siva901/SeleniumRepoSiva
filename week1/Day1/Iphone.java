65d849662ac9b622df7ee2c7300c2dcc1c9ec175
Siva Jothi <89371435+Siva901@users.noreply.github.com>
2023-05-17 20:23:16 +0530

    week1

---
+++ week1/Day1/Iphone.java
@@ -0,0 +1,14 @@
+package week1.Day1;
+
+public class Iphone {
+	
+	
+	public void makeCall() {
+		System.out.println("Make a Call");
+	}
+	public void model() {
+		System.out.println("Model");
+	}
+
+	
+}
---
+++ week1/Day1/Smartphone.java
@@ -0,0 +1,12 @@
+package week1.Day1;
+
+public class Smartphone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Iphone iphone=new Iphone();
+		iphone.makeCall();
+		iphone.model();
+	}
+
+}
